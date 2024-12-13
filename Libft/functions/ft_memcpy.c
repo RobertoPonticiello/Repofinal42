748e82e49ded221b35dae327771bcee968e233c2
Roberto nicola Ponticiello <rpontici@e3r12p8.42roma.it>
2024-12-13 15:58:35 +0100

    bella pe te

--- Libft/functions/ft_memcpy.c
+++ Libft/functions/ft_memcpy.c
@@ -1,13 +1,17 @@
 void	*ft_memcpy(void *dest, const void *src, size_t count)
 {
 	size_t	i;
+	char	*d;
+	const char	s*;
 
 	i = 0;
+	*d = (char *)dest;
+	*s = (const char *)src;
 
 	while (i < count)
 	{
-		dest[i] = src[i];
+		d[i] = s[i];
 		i++;
 	}
-	return
+	return(dest);
 }
