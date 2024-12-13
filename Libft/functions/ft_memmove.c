4d9ba9b903e63d9dac9b537ebc06b7b4dd88f123
Roberto nicola Ponticiello <rpontici@e3r12p8.42roma.it>
2024-12-13 18:24:02 +0100

    bella floski

---
+++ Libft/functions/ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpontici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 17:49:16 by rpontici          #+#    #+#             */
+/*   Updated: 2024/12/13 18:22:04 by rpontici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+	char	*d;
+	const char *s;
+
+	d = (char *)dest;
+	s = (const char *)src;
+	i = n;
+	if (d > s && d < s + n)
+	{
+		while (i > 0)
+		{
+			d[i - 1] = s[i - 1];
+			i--;
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < n)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return(dest);
+}
