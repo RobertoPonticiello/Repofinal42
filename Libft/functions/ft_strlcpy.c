f916a95a571d5ea2840697a5c25751421266fccd
Roberto nicola Ponticiello <rpontici@e3r12p8.42roma.it>
2024-12-13 21:04:35 +0100

    bo

---
+++ Libft/functions/ft_strlcpy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpontici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/13 20:55:23 by rpontici          #+#    #+#             */
+/*   Updated: 2024/12/13 21:04:14 by rpontici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char*	ft_strcpy(char* destination, const char* source)
+{
+	int	i;
+
+	i = 0;
+	while (source[i])
+	{
+		destination[i] = source[i];
+		i++;
+	}
+	destination[i] = '\0';
+	return(destination);
+}
