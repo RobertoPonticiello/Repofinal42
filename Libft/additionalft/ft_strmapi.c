74d0e0ecda28b27461b9803a37e207e194e646a7
rpontici <rpontici@Ponticiello-PC.>
2024-12-18 13:28:33 +0100

    e vafammok

---
+++ Libft/additionalft/ft_strmapi.c
@@ -0,0 +1,23 @@
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	unsigned int	i;
+	char	*str;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	str = malloc(sizeof(char) * (i + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return(str);
+}
