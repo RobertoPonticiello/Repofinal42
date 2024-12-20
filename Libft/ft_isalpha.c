1c7a0bd70bf4336dd814265457e174fdbe229a70
rpontici <rpontici@Ponticiello-PC.>
2024-12-20 09:57:07 +0100

    euric

--- Libft/ft_isalpha.c
+++ Libft/ft_isalpha.c
@@ -1,9 +1,16 @@
+
 #include "libft.h"
 
-int	ft_isalpha(char c)
+int	ft_isalpha(int c)
 {
-	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-		return(1);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
 	else
-		return(0);
+		return (0);
 }
