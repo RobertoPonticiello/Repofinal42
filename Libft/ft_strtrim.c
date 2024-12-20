4a62e39468bb6bb107431f2e3a3c455756f6a762
Roberto nicola Ponticiello <rpontici@e4r9p3.42roma.it>
2024-12-20 17:37:11 +0100

    dajeee

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rpontici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: rpontici <rpontici@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/20 13:46:13 by rpontici          #+#    #+#             */
-/*   Updated: 2024/12/20 13:46:15 by rpontici         ###   ########.fr       */
+/*   Updated: 2024/12/20 17:19:40 by rpontici         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,28 @@ static int	ft_is_in_set(char c, char const *set)
 	return (0);
 }
 
+static size_t	ft_start_index(char const *s1, char const *set)
+{
+	size_t	start;
+
+	start = 0;
+	while (s1[start] && ft_is_in_set(s1[start], set))
+		start++;
+	return (start);
+}
+
+static size_t	ft_end_index(char const *s1, char const *set, size_t start)
+{
+	size_t	end;
+
+	end = start;
+	while (s1[end])
+		end++;
+	while (end > start && ft_is_in_set(s1[end - 1], set))
+		end--;
+	return (end);
+}
+
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start;
@@ -32,15 +54,9 @@ char	*ft_strtrim(char const *s1, char const *set)
 
 	if (!s1 || !set)
 		return (NULL);
-	start = 0;
-	while (s1[start] && ft_is_in_set(s1[start], set))
-		start++;
-	end = start;
-	while (s1[end])
-		end++;
-	while (end > start && ft_is_in_set(s1[end - 1], set))
-		end--;
-	trimmed_str = (char *)malloc(sizeof(char) * (end - start + 1));
+	start = ft_start_index(s1, set);
+	end = ft_end_index(s1, set, start);
+	trimmed_str = malloc(sizeof(char) * (end - start + 1));
 	if (!trimmed_str)
 		return (NULL);
 	i = 0;
