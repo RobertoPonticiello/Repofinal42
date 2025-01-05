d366310347790e3eac2677fa81bf2331d8b4b465
Roberto nicola Ponticiello <rpontici@e4r2p1.42roma.it>
2025-01-05 22:25:07 +0100

    dai che ci siamo

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpontici <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/05 21:52:00 by rpontici          #+#    #+#             */
+/*   Updated: 2025/01/05 21:53:20 by rpontici         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -12,13 +24,15 @@
 
 typedef struct s_fdata
 {
-	char buffer[BUFFER_SIZE];
-	size_t start;
-	size_t end;
-} t_fdata;
+	char	buffer[BUFFER_SIZE];
+	size_t	start;
+	size_t	end;
+}	t_fdata;
 
 size_t	ft_strlen(const char *str);
 void	ft_clearindex(t_fdata *index);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	set_i(t_fdata *index);
 t_fdata	*ft_newindex(void);
 t_fdata	*ft_updateindex(t_fdata *index, int fd);
 char	*ft_readline(char *str, t_fdata *index);
